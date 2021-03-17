4fbb45edf8b8eb935317e97038e571883656c12a
Sergio Tijero <54224051+SergioTijero@users.noreply.github.com>
2021-03-16 19:38:19 -0500

    Add files via upload

---
+++ places/PE.php
@@ -0,0 +1,337 @@
+<?php
+
+/**
+ * Perú places
+ *
+ * @author   Juan Carlos Sergio Tijero Yupanqui <contacto@sergiotijero.com>
+ * @version  2.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+global $places;
+
+
+
+
+
+$places['PE'] = array(
+	'AMA' => array(
+        "Chachapoyas",
+        "Bagua",
+        "Bongará",
+        "Condorcanqui",
+        "Luya",
+        "Rodríguez de Mendoza",
+        "Utcubamba"
+		),
+	'ANC' => array(
+        "Aija",
+        "Antonio Raymondi",
+        "Asunción",
+        "Bolognesi",
+        "Carhuaz",
+        "Carlos F. Fitzcarrald",
+        "Casma",
+        "Corongo",
+        "Huaraz",
+        "Huari",
+        "Huarmey",
+        "Huaylas",
+        "Mariscal Luzuriaga",
+        "Ocros",
+        "Pallasca",
+        "Pomabamba",
+        "Recuay",
+        "Santa",
+        "Sihuas",
+        "Yungay"
+		),
+	'APU' => array(
+
+        "Abancay",
+        "Andahuaylas",
+        "Antabamba",
+        "Aymaraes",
+        "Cotabambas",
+        "Chincheros",
+        "Grau"
+		),
+	'ARE' => array(
+        "Alto Selva Alegre",
+        "Arequipa",
+        "Cayma",
+        "Cerro Colorado",
+        "Characato",
+        "Chiguata",
+        "Jacobo Hunter",
+        "José Luis Bustamante y Rivero",
+        "La Joya",
+        "Mariano Melgar",
+        "Miraflores",
+        "Mollebaya",
+        "Paucarpata",
+        "Pocsi",
+        "Polobaya",
+        "Quequeña",
+        "Sabandía",
+        "Sachaca",
+        "San Juan de Siguas",
+        "San Juan de Tarucani",
+        "Santa Isabel de Siguas",
+        "Santa Rita de Siguas",
+        "Socabaya",
+        "Tiabaya",
+        "Uchumayo",
+        "Vitor",
+        "Yanahuara",
+        "Yarabamba",
+        "Yura"
+		),
+	'AYA' => array(
+        "Huamanga",
+        "Cangallo",
+        "Huancasancos",
+        "Huanta",
+        "La Mar",
+        "Lucanas	Puquio",
+        "Parinacochas",
+        "Páucar del Sara Sara",
+        "Sucre",
+        "Víctor Fajardo",
+        "Vilcashuamán"
+		),
+	'CAJ' => array(
+        "Cajamarca",
+        "Cajabamba",
+        "Celendín",
+        "Contumazá",
+        "Cutervo",
+        "Chota",
+        "Hualgayoc",
+        "Jaén",
+        "Santa Cruz",
+        "San Miguel",
+        "San Ignacio",
+        "San Marcos",
+        "San Pablo"
+		),
+	'CAL' => array(
+        "Bellavista",
+        "Callao",
+        "Carmen de La Legua Reynoso",
+        "La Perla",
+        "La Punta",
+        "Ventanilla"
+		),
+	'CUS' => array(
+        "Cusco",
+        "Acomayo",
+        "Anta",
+        "Calca",
+        "Canas",
+        "Canchis",
+        "Chumbivilcas",
+        "Espinar",
+        "La Convención",
+        "Paruro	Paruro",
+        "Paucartambo",
+        "Quispicanchi",
+        "Urubamba"
+		),
+	'HUC' => array(
+        "Huánuco",
+        "Ambo",
+        "Dos de Mayo",
+        "Huacaybamba",
+        "Huamalíes",
+        "Leoncio Prado",
+        "Marañón",
+        "Pachitea",
+        "Puerto Inca",
+        "Lauricocha",
+        "Yarowilca"
+		),
+	'HUV' => array(
+        "Huancavelica",
+        "Acobamba",
+        "Angaraes",
+        "Castrovirreyna",
+        "Churcampa",
+        "Huaytará",
+        "Tayacaj"
+		),
+	'ICA' => array(
+        "Ica",
+        "Chincha",
+        "Nazca",
+        "Palpa",
+        "Pisc"
+		),
+	'JUN'  => array(
+        "Huancayo",
+        "Concepción",
+        "Chanchamayo",
+        "Jauja",
+        "Junín",
+        "Satipo",
+        "Tarma",
+        "Yauli",
+        "Chupac"
+		),
+	'LAL' => array(
+        "Trujillo",
+        "Ascope",
+        "Bolívar",
+        "Chepén",
+        "Julcán",
+        "Otuzco",
+        "Pacasmayo",
+        "Pata",
+        "Sánchez Carrión",
+        "Santiago de Chuco",
+        "Gran Chimú",
+        "Virú"
+		),
+	'LAM' => array(
+        "Chiclayo",
+        "Ferreñafe",
+        "Lambayeque"
+		),
+	'LIM' => array(
+        "Barranca",
+        "Cajatambo",
+        "Canta",
+        "Cañete",
+        "Huaral",
+        "Huarochirí",
+        "Huaura",
+        "Oyón",
+        "Yauyos"
+		),
+	'LMA' => array(
+        "Ancón",
+        "Ate",
+        "Ate Vitarte (Carretera Cental)",
+        "Barranco",
+        "Breña",
+        "Carabayllo",
+        "Chaclacayo",
+        "Chosica",
+        "Chorrillos",
+        "Comas",
+        "Cercado",
+        "Cieneguilla",
+        "El Agustino",
+        "Independencia",
+        "Jesús María",
+        "La Molina",
+        "La Victoria",
+        "Lima",
+        "Lince",
+        "Los Olivos",
+        "Lurín",
+        "Magdalena",
+        "Miraflores",
+        "Pachacamac",
+        "Pucusana",
+        "Pueblo Libre",
+        "Puente Piedra",
+        "Punta Hermosa",
+        "Punta Negra",
+        "Rimac",
+        "San Bartolo",
+        "San Borja",
+        "San Isidro",
+        "San Juan de Lurigancho",
+        "San Juan de Miraflores",
+        "San Luis",
+        "San Martín de Porres",
+        "San Miguel",
+        "Santa Anita",
+        "Santa Rosa",
+        "Salamanca",
+        "Santa María del Mar",
+        "Surco",
+        "Surquillo",
+        "Villa el Salvador",
+        "Villa María del Triunfo"
+		),
+	'LOR' => array(
+        "Maynas",
+        "Alto Amazonas",
+        "Loreto",
+        "Mariscal Ramón Castilla",
+        "Requena",
+        "Ucayali",
+        "Datem del Marañón",
+        "Putumay"
+		),
+	'MDD' => array(
+        "Tambopata",
+        "Manu",
+        "Tahuamanu"
+		),
+	'MOQ' => array(
+        "Mariscal Nieto",
+        "General Sánchez Cerro",
+        "Ilo"
+		),
+	'PAS' => array(
+        "Pasco",
+        "Daniel A. Carrión",
+        "Oxapampa"
+		),
+	'PIU' => array(
+        "Piura",
+        "Ayabaca",
+        "Huancabamba",
+        "Morropón",
+        "Paita",
+        "Sullana",
+        "Talara",
+        "Sechur"
+		),
+	'PUN' => array(
+        "Puno",
+        "San Román",
+        "Azángaro",
+        "Chucuito",
+        "El Collao",
+        "Melgar",
+        "Carabaya",
+        "Huancané",
+        "Sandia",
+        "San Antonio de Putina",
+        "Lampa",
+        "Yunguyo",
+        "Moh"
+		),
+	'SAM' => array(
+        "Moyobamba",
+        "Bellavista",
+        "El Dorado",
+        "Huallaga",
+        "Lamas",
+        "Mariscal Cáceres",
+        "Picota",
+        "Rioja",
+        "San Martín",
+        "Tocache"
+		),
+	'TAC' => array(
+        "Candarave",
+        "Jorge Basadre",
+        "Tacna",
+        "Tarata"
+		),
+	'TUM' => array(
+        "Tumbes",
+        "Zarumilla",
+        "Contralmirante Villar"
+		),
+	'UCA' => array(
+        "Coronel Portillo",
+        "Atalaya",
+        "Padre Abad",
+        "Purús"
+		)
+	);
---
+++ states/PE.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Perú states(provincias)
+ *
+ * @author   Juan Carlos Sergio Tijero Yupanqui <contacto@sergiotijero.com>
+ * @version  2.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states ['PE' ] = array (
+	'AMA' => 'Amazonas',
+	'ANC' => 'Ancash',
+	'APU' => 'Apurímac',
+	'ARE' => 'Arequipa',
+	'AYA' => 'Ayacucho',
+	'CAJ' => 'Cajamarca',
+	'CAL' => 'Callao',
+	'CUS' => 'Cusco',
+	'HUC' => 'Huánuco',
+	'HUV' => 'Huancavelica',
+	'ICA' => 'Ica',
+	'JUN' => 'Junín',
+	'LAL' => 'La Libertad',
+	'LAM' => 'Lambayeque',
+	'LIM' => 'Lima - Provincias',
+	'LMA' => 'Lima Metropolitana',
+	'LOR' => 'Loreto',
+	'MDD' => 'Madre de Dios',
+	'MOQ' => 'Moquegua',
+	'PAS' => 'Pasco',
+	'PIU' => 'Piura',
+	'PUN' => 'Puno',
+	'SAM' => 'San Martín',
+	'TAC' => 'Tacna',
+	'TUM' => 'Tumbes',
+	'UCA' => 'Ucayali',
+);
